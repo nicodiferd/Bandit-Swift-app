4b318942c03e290f4b08d4730b1373a6119c5e38
nicodiferd <nicodiferd@gmail.com>
2025-02-16 17:07:31 -0800

    V1.0 Bandit app finally in the making?

---
+++ Bandit/Bandit/BanditApp.swift
@@ -0,0 +1,17 @@
+//
+//  BanditApp.swift
+//  Bandit
+//
+//  Created by Nicolo DiFerdinando on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct BanditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bandit/BanditTests/BanditTests.swift
@@ -0,0 +1,17 @@
+//
+//  BanditTests.swift
+//  BanditTests
+//
+//  Created by Nicolo DiFerdinando on 2/16/25.
+//
+
+import Testing
+@testable import Bandit
+
+struct BanditTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
